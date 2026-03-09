a7eead568290027edcc01f6df096a596703bf6f6
JoseManuelRuiz <josruisoj@alu.edu.gva.es>
2026-03-09 11:07:38 +0100

    Modificado al fichero original

--- backend/class-agenda-backend/src/test/java/com/classagendag2/features/user/data/repository/JdbcUserRepositoryIT.java
+++ backend/class-agenda-backend/src/test/java/com/classagendag2/features/user/data/repository/JdbcUserRepositoryIT.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
 import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
@@ -30,7 +29,7 @@ class JdbcUserRepositoryIT {
 
         // si el codigo sobrevive y llega hasta aquí, significa que SI hay un .env local valido
         // inicializamos la fabrica, el DAO y finalmente el repositorio
-        userRepository = new JdbcUserRepository(new UserDao((Connection) new DbConnectionFactory()));
+        userRepository = new JdbcUserRepository(new UserDao(new DbConnectionFactory()));
     }
 
     @Test
